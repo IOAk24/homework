9f8be1953e81eaacacb392ee475f73124e694859
parker.nelson <invaildtag.ak@gmail.com>
2025-02-06 15:34:26 -0600

    inital commit

--- src/School/Course.java
+++ src/School/Course.java
@@ -1,7 +1,31 @@
 package School;
 
 public class Course {
-    public static void main(String[] args) {
+    private String department;
+    private int courseNumber;
+    private int creditHours;
 
+    public Course(String department, int courseNumber, int creditHours){
+        this.department = department;
+        this.courseNumber = courseNumber;
+        this.creditHours = creditHours;
+    }
+    public String getDepartment(){
+        return department;
+    }
+    public int getCourseNumber(){
+        return courseNumber;
+    }
+    public int getCreditHours(){
+        return creditHours;
+    }
+    public String toString(){
+        return "Course{department='" + department + "', courseNumber=" + courseNumber + ", creditHours=" + creditHours + "}";
+    }
+    public static void main(String[] args) {
+        Course course1 = new Course("Math", 12, 4);
+        Course course2 = new Course("English", 24, 4);
+        System.out.println(course1);
+        System.out.println(course2);
     }
 }
--- src/School/Student.java
+++ src/School/Student.java
@@ -1,4 +1,41 @@
 package School;
 
 public class Student {
+    private String Name;
+    private int IdNumber;
+    private static int nextID = 1;
+
+    public Student (String Name, int IdNumber){
+        this.Name = Name;
+        this.IdNumber = IdNumber;
+    }
+    public Student(String name) {
+        this.Name = name;
+        this.IdNumber = nextID;
+        nextID++;
+    }
+    public String getName(){
+        return Name;
+    }
+    public int getIdNumber(){
+        return IdNumber;
+    }
+    public void setName(String newName){
+        this.Name = newName;
+    }
+    public boolean precedesById(Student other) {
+        return this.IdNumber < other.IdNumber;
+    }
+    public String toString() {
+        return "Student{name='" + Name + "', idNumber=" + IdNumber + "}";
+    }
+
+    public static void main(String[] args) {
+        Student student1 = new Student("Parker Nelson", 123);
+        Student student2 = new Student("Kendrick Achebe", 456);
+        System.out.println(student1);
+        System.out.println(student2);
+        System.out.println("bob1 precedes bob2: " + student1.precedesById(student2)); // Should print true
+        System.out.println("bob2 precedes bob1: " + student2.precedesById(student1)); // Should print false
+    }
 }
